3848eceb066e4e3c585cd274301edeed3b3c3782
Miles Florence <milesflo15@gmail.com>
2016-10-02 16:30:26 -0700

    Added pseudocode

---
+++ grey-goo.lua
@@ -0,0 +1,34 @@
+-- Grey Goo
+-- by <コ:彡
+
+local tArgs = {...}
+
+if tArgs ~0 do
+	print("Usage: goo")
+	return
+end
+
+-- -- Turtle counter
+-- local count = 0
+-- -- Turtle ceiling
+-- local generations = tonumber(tArgs[1])
+
+-- if generations < 1 do
+-- 	print("Iteration count must be positive.")
+-- 	return
+-- end
+
+while true do
+	-- Excavate a tunnel (drop everything that doesn't aide in turtle creation)
+
+	-- If if can afford to craft a mining crafty turtle, do
+		-- 8 Iron ingots + 1 chest (8 wood planks) + 1 computer (8 Smoothstone + 1 redstone + 1 glass panel)
+		-- 3 diamonds + 2 sticks = Diamond Pick
+		-- 4 wood planks = Crafting table
+			-- This is so expensive!!!! Might invalidate the whole project
+			-- Another option is Ender Chests connected to a factory, but that's even more expensive
+
+
+	-- Place baby Mining Turtle behind it, turn it left or right, and run this script on it
+
+end
